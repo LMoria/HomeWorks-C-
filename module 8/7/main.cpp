f61cad69bd907a5db5c53f8edbdfe7d9c813c29e
LMoria <anasmanvit3@gmail.com>
2020-10-11 20:30:22 +0300

    Задача 7, Модуль 8 - done!

--- module 8/7/main.cpp
+++ module 8/7/main.cpp
@@ -19,21 +19,55 @@ int main() {
     int denomination2000 = 2000;
     int denomination5000 = 5000;
 
-    if(moneyIssue > 150000) {
-        cout << "Банкомат не может выдать такую сумму за раз. \n";
-    } else if(moneyIssue < 100 && moneyIssue % 100 != 0) {
+    //счётчики купюр
+    int counter100 = 0;
+    int counter200 = 0;
+    int counter500 = 0;
+    int counter1000 = 0;
+    int counter2000 = 0;
+    int counter5000 = 0;
+
+    if(moneyIssue > 150000 || moneyIssue < 100 && moneyIssue % 100 != 0 || moneyIssue / 10 % 10 > 0) {
         cout << "Недопустимая сумма для вывода средств. \n";
+        return 0;
     } else if(moneyIssue / denomination5000 != 0) {
-        cout << "Для выдачи данной суммы потребуется " << moneyIssue / denomination5000 << " купюр номиналом 5000р.\n";
-    } else if(moneyIssue / denomination2000 != 0 && moneyIssue % denomination2000 == 0) {
-        cout << "Для выдачи данной суммы потребуется " << moneyIssue / denomination2000 << " купюр номиналом 2000р.\n";
-    } else if(moneyIssue / denomination1000 != 0) {
-        cout << "Для выдачи данной суммы потребуется " << moneyIssue / denomination1000 << " купюр номиналом 1000р.\n";
-    } else if(moneyIssue / denomination500 != 0) {
-        cout << "Для выдачи данной суммы потребуется " << moneyIssue / denomination500 << " купюр номиналом 500р.\n";
-    } else if(moneyIssue / denomination200 != 0 && moneyIssue % denomination200 == 0) {
-        cout << "Для выдачи данной суммы потребуется " << moneyIssue / denomination200 << " купюр номиналом 200р.\n";
-    } else if(moneyIssue / denomination100 != 0) {
-        cout << "Для выдачи данной суммы потребуется " << moneyIssue / denomination100 << " купюр номиналом 100р.\n";
+        counter5000 = moneyIssue / denomination5000;
     }
+
+    //промежуточное
+    int without5000 = moneyIssue - (counter5000 * 5000);
+
+    if(without5000 / denomination2000 != 0) {
+        counter2000 = without5000 / denomination2000;
+    }
+
+    //промежуточное
+    int without2000 = without5000 - (counter2000 * 2000);
+
+    if(without2000 / denomination1000 != 0) {
+        counter1000 = without2000 / denomination1000;
+    }
+
+    //промежуточное
+    int without1000 = without2000 - (counter1000 * 1000);
+
+    if(without1000 / denomination500 != 0) {
+        counter500 = without1000 / denomination500;
+    }
+
+    //промежуточное
+    int without500 = without1000 - (counter500 * 500);
+
+    if(without500 / denomination200 != 0) {
+        counter200 = without500 / denomination200;
+    }
+
+    //промежуточное
+    int without200 = without500 - (counter200 * 200);
+
+    if(without200 / denomination100 != 0) {
+        counter100 = without200 / denomination100;
+    }
+
+    cout << "Для выдачи данной суммы потребуется " << counter5000 << " купюр номиналом 5000р, " << counter2000 << " купюр номиналом 2000р, " << counter1000 << " купюр номиналом 1000р, " << counter500 << " купюр номиналом 500р, " << counter200 << " купюр номиналом 200р, " << counter100 << " купюр номиналом 100р.\n";
 }
